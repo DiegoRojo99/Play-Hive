a68b14c57e11c393f500fbeb53f8b6aaeb8fff64
DiegoRojo99 <diego99rojo@gmail.com>
2024-11-30 00:21:58 +0000

    Remove axios

--- frontend/src/components/nav/NavBar.tsx
+++ frontend/src/components/nav/NavBar.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import UserProfile from './UserProfile';
 import SteamLoginButton from '../buttons/SteamLoginButton';
 
