ffacee5d79d9dbea09cd0249c338625a00bc2b70
DiegoRojo99 <diego99rojo@gmail.com>
2024-12-01 14:27:45 +0000

    Backend login and library works

--- backend/src/services/steamServices.ts
+++ backend/src/services/steamServices.ts
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 const STEAM_API_KEY = process.env.STEAM_API_KEY;
 
 /**
@@ -9,13 +12,11 @@ export async function fetchUserLibrary(steamID: string) {
   if (!steamID) {
     throw new Error('SteamID is required');
   }
+  if (!STEAM_API_KEY) {
+    throw new Error('STEAM_API_KEY is missing');
+  }
 
-  const url = `http://api.steampowered.com/IPlayerService/GetOwnedGames/v1/
-    ?key=${STEAM_API_KEY}
-    &steamid=${steamID}
-    &include_appinfo=1
-    &include_played_free_games=1
-  `;
+  const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${STEAM_API_KEY}&steamid=${steamID}&format=json&include_appinfo=1&include_played_free_games=1`
   const response = await fetch(url);
 
   if (!response.ok) {
