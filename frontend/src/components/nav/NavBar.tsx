b63cebfc1f16ed8eda01e30299181ffb9cb5a475
DiegoRojo99 <diego99rojo@gmail.com>
2024-12-03 14:45:00 +0000

    Added browser to the nav bar

--- frontend/src/components/nav/NavBar.tsx
+++ frontend/src/components/nav/NavBar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import UserProfile from './UserProfile';
 import SteamLoginButton from '../buttons/SteamLoginButton';
 import { useUser } from '../../contexts/UserContext';
+import { Link } from 'react-router-dom';
+import './Nav.css';
 
 const NavBar: React.FC = () => {
   const { user } = useUser();
@@ -9,7 +11,23 @@ const NavBar: React.FC = () => {
   return (
     <nav style={{ display: 'flex', width: '100%', justifyContent: 'space-between', padding: '10px 20px' }}>
       <div style={{ display: 'flex', alignItems: 'center', marginLeft: '16px' }}>Play Hive</div>
-      <div style={{ display: 'flex', alignItems: 'center', paddingRight: '16px' }}>
+      <Link to="/browser">Browser</Link>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div style={{ display: 'flex', alignItems: 'center', marginRight: '32px' }}>
         {user ? <UserProfile user={user} /> : <SteamLoginButton />}
       </div>
     </nav>
