65c733d0f29d85b77240c39a76c77a964c631c8a
DiegoRojo99 <diego99rojo@gmail.com>
2024-12-05 23:20:11 +0000

    Imported a loader

---
+++ frontend/src/components/extras/Loader.tsx
@@ -0,0 +1,17 @@
+import './Loader.css';
+
+function Loader(){
+  return (
+    <div className="loader">
+      <span></span>
+
+      <div id="dot-1" className="dot"></div>
+      <div id="dot-2" className="dot"></div>
+      <div id="dot-3" className="dot"></div>
+      <div id="dot-4" className="dot"></div>
+      <div id="dot-5" className="dot"></div>
+    </div>
+  )
+};
+
+export default Loader;
