b7ac6133f48615c1eb8513da8e2f2190ccaaf44b
DiegoRojo99 <diego99rojo@gmail.com>
2024-12-08 01:29:04 +0000

    Library now fetches user data

--- frontend/src/components/pages/library/Library.jsx
+++ frontend/src/components/pages/library/Library.jsx
@@ -1,56 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import { useUser } from '../../../contexts/UserContext';
-import './Library.css';
 import LibraryItem from './LibraryItem';
+import './Library.css';
 
 const Library = () => {
   const { user } = useUser();
-  const [library, setLibrary] = useState([]);
+  const [userSteamProfile, setUserSteamProfile] = useState(null);
+  const [library, setLibrary] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  
+  const fetchLibrary = async (steamId) => {
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/library?steamID=${steamId}`);
+      if (!response.ok) {
+        throw new Error(`Error: ${response.statusText}`);
+      }
+      const data = await response.json();
+
+      const gamesWithImages = await Promise.all(
+        data.map(async (game) => {
+          const gameResponse = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/steam/game/${game.appid}`);
+          const gameData = await gameResponse.json();
+          if (gameData[game.appid].success) {
+            const headerImage = gameData[game.appid].data.header_image;
+            return { ...game, header_image: headerImage };
+          }
+          return game;
+        })
+      );
+
+      setLibrary(gamesWithImages);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    if (!user) {
-      return;
-    }
-
-    if (!user.steamID) {
-      setError("Steam ID missing");
-      setLoading(false);
-      return;
-    }
-    const fetchLibrary = async () => {
+    const fetchUserSteamProfile = async () => {
       try {
-        const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/library?steamID=${user.steamID}`);
+        const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/user/steamProfile?userId=${user.userId}`);
         if (!response.ok) {
           throw new Error(`Error: ${response.statusText}`);
         }
-        const data = await response.json();
-
-        const gamesWithImages = await Promise.all(
-          data.map(async (game) => {
-            const gameResponse = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/steam/game/${game.appid}`);
-            const gameData = await gameResponse.json();
-            if (gameData[game.appid].success) {
-              const headerImage = gameData[game.appid].data.header_image;
-              return { ...game, header_image: headerImage };
-            }
-            return game;
-          })
-        );
-
-        setLibrary(gamesWithImages);
+        const steamProfile = await response.json();
+        setUserSteamProfile(steamProfile);
+        fetchLibrary(steamProfile.steamId);
       } catch (err) {
         setError(err.message);
       } finally {
         setLoading(false);
       }
-    };
+    };   
+    
+    if(user){
+      fetchUserSteamProfile();
+    }
 
-    fetchLibrary();
   }, [user]);
 
-  if (loading) {
+  if (loading || !library) {
     return <div>Loading library...</div>;
   }
 
@@ -60,7 +71,7 @@ const Library = () => {
 
   return (
     <div id="library">
-      <h1>{`${user?.username}'s Library`}</h1>
+      <h1>{`${userSteamProfile?.username}'s Library`}</h1>
       <div className="library-grid">
         {library.length === 0 ? ( <p>No games in library</p> ) : 
           library.map((game) => 
