ec05e78f8429810b5a27218c1e8984e44e6fca89
DiegoRojo99 <diego99rojo@gmail.com>
2024-12-09 23:11:07 +0000

    Created endpoint for sending user and game achievements

---
+++ backend/src/routes/achievementRoutes.ts
@@ -0,0 +1,37 @@
+import express from 'express';
+import { fetchGameAchievements, fetchUserAchievements } from '../services/achievementServices';
+
+const router = express.Router();
+
+router.get('/:gameId', async (req, res): Promise<any> => {
+  const { gameId } = req.params;
+  const { userId } = req.query;
+
+  if (!gameId || !userId) {
+    return res.status(400).send('Game ID and User ID are required.');
+  }
+
+  if (isNaN(Number(gameId))) {
+    return res.status(400).send('Game ID should be a number.');
+  }
+
+  try {
+    const gameAchievementsPromise = fetchGameAchievements(Number(gameId));
+    const userAchievementsPromise = fetchUserAchievements(Number(gameId), String(userId));
+
+    const [gameAchievements, { userAchievements, gameName }] = await Promise.all([
+      gameAchievementsPromise,
+      userAchievementsPromise,
+    ]);
+
+    res.status(200).json({
+      gameAchievements,
+      userAchievements,
+      gameName,
+    });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
