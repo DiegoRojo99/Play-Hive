e130420ba7b369bdae6186d29183a0671d635f61
DiegoRojo99 <diego99rojo@gmail.com>
2024-12-10 18:18:25 +0000

    Added user cached library

--- backend/src/services/steamServices.ts
+++ backend/src/services/steamServices.ts
@@ -1,4 +1,7 @@
 import dotenv from 'dotenv';
+import NodeCache from 'node-cache';
+
+const gameCache = new NodeCache({ stdTTL: 86400, checkperiod: 3600 });
 dotenv.config();
 
 const STEAM_API_KEY = process.env.STEAM_API_KEY;
@@ -16,6 +19,14 @@ export async function fetchUserLibrary(steamID: string) {
     throw new Error('STEAM_API_KEY is missing');
   }
 
+  const cacheKey = `library_${steamID}`;
+  const cachedData: any | undefined = gameCache.get(cacheKey);
+  
+  if (cachedData) {
+    return cachedData;
+  }
+
+
   const url = `https://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=${STEAM_API_KEY}&steamid=${steamID}&format=json&include_appinfo=1&include_played_free_games=1`
   const response = await fetch(url);
 
@@ -24,7 +35,9 @@ export async function fetchUserLibrary(steamID: string) {
   }
 
   const data = await response.json();
-  return data.response?.games || [];
+  let library = data.response?.games || [];
+  gameCache.set(cacheKey, library);
+  return library;
 }
 
 
